8552670c17733118d8aedd7fe83a62b7acb8f644
Remo Zumsteg <remolueoend@users.noreply.github.com>
2017-12-07 01:52:40 +0100

    add initial arduino source.

---
+++ arduino/src/main.cpp
@@ -0,0 +1,89 @@
+#include <Arduino.h>
+#include <VirtualWire.h>
+
+const int transmit_pin = 2;
+const int receive_pin = 2;
+
+void setup_tx () {
+  vw_set_tx_pin(transmit_pin);
+  vw_setup(2000);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void setup_rx () {
+  Serial.begin(9600);	// Debugging only
+  Serial.println("setup");
+  vw_set_rx_pin(receive_pin);
+  vw_setup(2000);
+  vw_rx_start();
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void setup_rx_simple() {
+  Serial.begin(9600);
+  pinMode(receive_pin, INPUT);
+}
+
+void rx_simple() {
+  int data = digitalRead(receive_pin);
+  Serial.println(data);
+  delay(10);
+}
+
+byte count = 1;
+
+void tx() {
+  char msg[7] = {'h','e','l','l','o',' ','#'};
+  msg[6] = count;
+  digitalWrite(LED_BUILTIN, HIGH); // Flash a light to show transmitting
+  vw_send((uint8_t *)msg, 7);
+  vw_wait_tx(); // Wait until the whole message is gone
+  digitalWrite(LED_BUILTIN, LOW);
+  count = count + 1;
+  delay(100);
+}
+
+void rx () {
+  uint8_t buf[VW_MAX_MESSAGE_LEN];
+  uint8_t buflen = VW_MAX_MESSAGE_LEN;
+
+  /*
+  int msg_rec = vw_wait_rx_max(5000);
+  if(!msg_rec) {
+    Serial.println("No message received in 5000ms");
+    return;
+  } else {
+    Serial.print("Message received");
+    Serial.println(msg_rec);
+  }
+  */
+
+  if (vw_get_message(buf, &buflen)) {
+  	int i;
+    digitalWrite(LED_BUILTIN, HIGH); // Flash a light to show received good message
+  	// Message with a good checksum received, print it.
+  	Serial.print("Got: ");
+  	for (i = 0; i < buflen; i++) {
+      Serial.print(buf[i], HEX);
+      Serial.print(' ');
+  	}
+  	Serial.println();
+    digitalWrite(LED_BUILTIN, LOW);
+  } else {
+    Serial.println("Invalid message:");
+    int i;
+    for (i = 0; i < buflen; i++) {
+      Serial.print(buf[i], HEX);
+      Serial.print(' ');
+  	}
+    Serial.println(' ');
+  }
+}
+
+void setup() {
+  setup_rx_simple();
+}
+
+void loop() {
+  rx_simple();
+}
